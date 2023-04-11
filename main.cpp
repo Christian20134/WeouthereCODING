06513a5e116d31994fb07631105bfe6c88dd6254
oki <gtracer465@gmail.cim>
2023-04-11 14:17:02 -0400

    player class added with basic stats

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Player {
+    int hp;
+    int maxHP;
+    int damage;
+    float accuracy;
+    int armor;
+    int inventory[];
+public:
+    Player() {
+        maxHP = 10;
+        hp = maxHP;
+        damage = 1;
+        accuracy = 0.9;
+        armor = 0;
+        cout << "Object of class Player created. ";
+    }
+};
 
 int main() {
-    std::cout << "I done changed some shit ong fr cuh" << std::endl;
+    Player mainPlayer;
     return 0;
 }
 
