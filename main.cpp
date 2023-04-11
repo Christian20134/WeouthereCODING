7529798739490bc18cda6e0510970258647762f5
oki <gtracer465@gmail.cim>
2023-04-11 14:27:04 -0400

    enemy class, encounter funct prototype

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <string>
 using namespace std;
 
-class Player {
+void enemyEncounter() {}; /// Encounter/battle function prototype
+
+class Player { /// Player class, constructor values exist because this class is only created
+               /// when the game is started.
     int hp;
     int maxHP;
     int damage;
@@ -17,6 +20,21 @@ public:
         accuracy = 0.9;
         armor = 0;
         cout << "Object of class Player created. ";
+    }
+    friend void enemyEncounter();
+};
+
+class Enemy { /// Template for enemies in encounters
+    int hp;
+    int damage;
+    int armor;
+    float accuracy;
+public:
+    Enemy(int X, int Y, int Z, float J) {
+        hp = X;
+        damage = Y;
+        armor = Z;
+        accuracy = J;
     }
 };
 
