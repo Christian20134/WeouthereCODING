e2b1b14dc0e0fe6f1d0c0de7ebaa81af7f25d37c
Oki <gtracer465@gmail.com>
2023-04-17 23:49:38 -0400

    Second paragraph of dialogue, updated text function for variable delay, added hard-coded transition animation

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@
 using namespace std;
 
 /* Dialogue storage */
-struct Dialogue {
-    string introText = "This is a tale of adventure, YOUR tale of adventure to find the fabled fountain of youth located deep within the Everglades. Many perils await you, brave explorer, twists and turns only the mind can fathom. Do you have what it takes to brave the wild? Or will you fail and never reach the glory you're striving for?";
-
+struct Dialogue { /// Scene #, dialogue #
+    string introText = "This is a tale of adventure, YOUR tale of adventure to find the fabled fountain of youth located"
+                       " deep within the Everglades. Many perils await you, brave explorer, twists and turns only the "
+                       "mind can fathom. Do you have what it takes to brave the wild? Or will you fail and never reach "
+                       "the glory you're striving for?";
+    string scene1d1 = "Facing you are the harsh marshes of the Everglades. You were told by your late spelunking "
+                      "grandfather that he found some hints on a text scroll found while exploring a cave near a "
+                      "village. The scroll says that the heart of the Everglades contains a temple buried underneath, "
+                      "lost to the sands of time. However, the explorer that finds the temple will be rewarded with "
+                      "eternal life.";
 };
 
 
@@ -72,25 +79,40 @@ void enemyEncounter(Player& player, Enemy& enemy) {                             
     } while (enemy.hp >= 0);
 }
 
-void displayText(string& displayedText) {
+void displayText(string& displayedText, int delay) { /// Takes string input and delay between chars in ms.
     int colCounter = 0;
     for (int i = 0; i < displayedText.length(); i++) {
         cout << displayedText[i];
         colCounter++;
-        this_thread::sleep_for(chrono::milliseconds(1));
-        if (displayedText[i] == ' ' && colCounter > 112) {
+        this_thread::sleep_for(chrono::milliseconds(delay));
+        if (displayedText[i] == ' ' && colCounter > 110) {
             cout << " ";
             cout << endl;
             colCounter = 0;
         } else continue;
     }
+    cout << endl;
+    this_thread::sleep_for(chrono::milliseconds(500));
 }
+
+void transition() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << ".";
+            this_thread::sleep_for(chrono::milliseconds(1));
+        }
+        cout << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+};
 
 
 int main() {
     Player mainPlayer;
     Dialogue dialogue;
-    displayText(dialogue.introText);
+    displayText(dialogue.introText, 1);
+    transition();
+    displayText(dialogue.scene1d1, 1);
     return 0;
 }
 
