87bdac5aa7f859f3447f59e54845ca1b0417687a
Oki <gtracer465@gmail.com>
2023-04-18 20:04:35 -0400

    More dialogue, choice logic, macros

--- main.cpp
+++ main.cpp
@@ -6,19 +6,39 @@ using namespace std;
 
 /* Text storage */
 struct Text { /// Scene #, dialogue #, choice #
+    string userPromptBegin = "What will you do?";
+
+    string badInput = "Traveler, I do not understand your command. (Input any of the numbers given.)";
+
+    string breakText = "Press ENTER to continue...";
+
     string introText = "This is a tale of adventure, YOUR tale of adventure to find the fabled fountain of youth located"
                        " deep within the Everglades. Many perils await you, brave explorer, twists and turns only the "
                        "mind can fathom. Do you have what it takes to brave the wild? Or will you fail and never reach "
                        "the glory you're striving for?";
-    string scene1intro = "Facing you are the harsh marshes of the Everglades. You were told by your late spelunking "
+
+    string scene1d1 = "Facing you are the harsh marshes of the Everglades. You were told by your late spelunking "
                       "grandfather that he found some hints on a text scroll found while exploring a cave near a "
                       "village. The scroll says that the heart of the Everglades contains a temple buried underneath, "
                       "lost to the sands of time. However, the explorer that finds the temple will be rewarded with "
                       "eternal life.";
-    string userPromptBegin = "What will you do?";
-    string scene1d1c1 = "1. Check the scroll \n 2. Set off on your adventure";
-};
 
+    string scene1d1c = "1. Check the scroll \n2. Set off on your adventure";
+
+    string scene1d1c1 = "\"Heart of the marshes is where you go, treasure hidden deep down below, Entry guarded a riddle "
+                        "again, place the gems in order of the cities within.\"";
+
+    string scene1d1c2 = "You take your first steps into the marsh. It's humid, and you are surrounded by trees all attached "
+                        "to the water you walk in currently. You shift carefully, as any gator in the area could easily "
+                        "stop you in your tracks and end your adventure permanently. However, you chose a specific time "
+                        "they are less active. Specifically, the afternoon, as the temperature drop makes them less active. "
+                        "Nevertheless, you pressed on, finding reprieve in the fact that you remembered a handgun, although"
+                        "only eight bullets could fit alongside it.\n";
+
+    string scene1d2 = "\nComing to a fork in the glades, you have two choices: a path to the right that looks traveled "
+                      "at a glance, with a clear line to follow through - perhaps it leads somewhere? Or, a path to the "
+                      "left that seems less traveled, and may lead you astray.";
+};
 
 /* Prototypes */
 class Enemy;
@@ -26,6 +46,7 @@ class Player;
 void enemyEncounter(Player& player, Enemy& enemy); /// Encounter/battle function
 void gameOver(); /// If you die
 void displayText(string& displayedText, int delay, int interLineDelay);
+int readInput(Text& text);
 
 
 /* Class Definitons */
@@ -56,6 +77,9 @@ public:
         if (hp <= 0) {
             gameOver();
         }
+    }
+    void setScrollStatus() {
+        scrollChecked = 1;
     }
     friend void enemyEncounter(Player& player, Enemy& enemy);
 };
@@ -96,7 +120,9 @@ void displayText(string& displayedText, int delay, int interLineDelay) { /// Tak
             cout << endl;
             colCounter = 0;
             this_thread::sleep_for(chrono::milliseconds(interLineDelay));
-        } else continue;
+        } else if (displayedText[i] == '!' || displayedText[i] == '.' || displayedText[i] == ',' || displayedText[i] == '?' || displayedText[i] == '-') {
+            this_thread::sleep_for(chrono::milliseconds(delay + 200));
+        }
     }
     cout << endl;
     this_thread::sleep_for(chrono::milliseconds(500));
@@ -113,12 +139,45 @@ void transition() {
     }
 };
 
-int choice(Text& text) {
+void textBreak(Text& text) {
+    displayText(text.breakText, 1, 1);
+    cin.get();
+    cin.ignore(1, '\n');
+}
+
+int choice(Text& text) {                                                                                                /// Find a way to make this a thing. Low priority
     displayText(text.userPromptBegin, 1, 250);
 }
 
-int scene1(Text& text) {
-    displayText(text.scene1intro, 1,1);
+void whatWillYouDo(Text& text) {
+    displayText(text.userPromptBegin, 1, 250);
+}
+
+int scene1(Text& text, Player& player) {
+    int choice1 = 0;
+    displayText(text.scene1d1, 1, 1);
+    whatWillYouDo(text);
+    displayText(text.scene1d1c, 1, 250);
+    switch (readInput(text)) {
+        case 1:
+            displayText(text.scene1d1c1, 20, 250);
+            player.setScrollStatus();
+            textBreak(text);
+        case 2:
+            displayText(text.scene1d1c2, 1, 1);
+            displayText(text.scene1d2, 1, 1);
+            break;
+    }
+}
+
+int readInput(Text& text) {
+    int choice = 0;
+    cin >> choice;
+    while (choice != 1 && choice != 2 && choice != 3) {
+        displayText(text.badInput, 1, 1);
+        cin >> choice;
+    }
+    return choice;
 }
 
 int main() {
@@ -126,7 +185,7 @@ int main() {
     Text text;
     displayText(text.introText, 1, 1);
     transition();
-    scene1(text);
+    scene1(text, mainPlayer);
     return 0;
 }
 
